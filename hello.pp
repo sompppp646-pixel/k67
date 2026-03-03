17c67c8bf05eca79f2ddfc18edf3ec624b147355
sompppp646-pixel <sompppp646@gmail.com>
2026-03-03 09:31:28 +0700

    Create hello.pp

---
+++ hello.pp
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Page Title</title>
+</head>
+<body>
+
+<h1 style="background-color:MediumSeaGreen;">em tên là somphasone</h1>
+<p style="background-color:DodgerBlue;">lớp CNTT K67.</p>
+
+</body>
+</html>
+
+
